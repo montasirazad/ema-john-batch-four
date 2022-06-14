29f3c05da046e32829deee7519c824fd192522a3
unknown <montasirazad@gmail.com>
2022-06-14 17:59:02 +0600

    price toFixed() added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,20 +2,25 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = (props) => {
-    const  cart = props.cart;
+    const cart = props.cart;
 
     let total = 0;
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
         total = total + product.price;
     }
- 
+    const shipping = 15;
+    const tax = (total) * .1;
+    const grandTotal = total + shipping + tax;
 
     return (
         <div>
             <h3>Order summary</h3>
             <h3>items ordered: {cart.length}</h3>
             <p>Total: {total}</p>
+            <p><small>Shipping charge: {shipping}</small></p>
+            <p><small>Tax: {tax.toFixed(2)}</small></p>
+            <p>Total price: {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
